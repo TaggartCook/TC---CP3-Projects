3ce1c1bcc52f15f9abb0e54b2cf6316c93871110
TaggartCook <taggart.cook@ucas-edu.net>
2024-10-10 11:32:02 -0600

    added finished projects folder

--- orderUp.py
+++ orderUp.py
@@ -1,11 +1,12 @@
 class Order:
-    def __init__(self, menu, drink, appetizer, mainCourse, dessert, sides=[]):
+    def __init__(self, menu, drink, appetizer, mainCourse, dessert, sideOne, sideTwo):
         self.menu = menu
         self.drink = drink
         self.appetizer = appetizer
         self.mainCourse = mainCourse
         self.dessert = dessert
-        self.sides = sides
+        self.sideOne = sideOne
+        self.sideTwo = sideTwo
 
     def __str__(self):
             return f"You have ordered {self.appetizer}, {self.mainCourse}, {self.drink}, with a side of {self.sides}, and {self.dessert}"
@@ -26,5 +27,8 @@ class Order:
     def getDes(self):
         print()
 
-    def getSid(self):
+    def getSOne(self):
+        print()
+
+    def getSTwo(self):
         print()
