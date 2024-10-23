48e449de12b5990c2726e95fc2394cb4ffcebe98
TaggartCook <taggart.cook@ucas-edu.net>
2024-10-23 11:49:07 -0600

    this is so weirddddd

---
+++ equations.py
@@ -0,0 +1,11 @@
+def add(numOne, numTwo):
+    return numOne + numTwo
+
+def sub(numOne, numTwo):
+    return numOne - numTwo
+
+def mul(numOne, numTwo):
+    return numOne * numTwo
+
+def div(numOne, numTwo):
+    return numOne / numTwo
